f59b23ee5cff294301c14a15793a424fdd9b3066
Ugo Mmirikwe <ugommirikwe@gmail.com>
2022-01-27 09:17:09 +0200

    Initial Commit

---
+++ WebviewApp/WebviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewAppApp.swift
+//  WebviewApp
+//
+//  Created by Ugochukwu Mmirikwe on 2022/01/27.
+//
+
+import SwiftUI
+
+@main
+struct WebviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
